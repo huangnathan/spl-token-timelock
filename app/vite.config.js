1b1e3f9dc74a0ac2e5e40d80b3f5d587f2c9c1b0
yooxp <937212680@qq.com>
2022-02-07 17:34:11 +0800

    add eslint config

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import eslintPlugin from 'vite-plugin-eslint';
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    eslintPlugin(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
